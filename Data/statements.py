f6993f4f5b990d4d71e403bf96e1e00a2bdd57f5
Ekimerton <ekim0252@gmail.com>
2019-11-08 17:34:25 -0500

    Created test.py, improving statements.py

--- Data/statements.py
+++ Data/statements.py
@@ -1,21 +1,34 @@
 # Simple program that tries out the financialmodelprep API.
 import requests
+statement_params = {'period': 'quarter'}
 
 def income_statement(ticker):        
     url = "https://financialmodelingprep.com/api/v3/financials/income-statement/" + ticker
-    resp = requests.get(url=url)
+    resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
 
 def balance_sheet(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/" + ticker
-    resp = requests.get(url=url)
+    resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
 
 def cash_flow(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/" + ticker
-    resp = requests.get(url=url)
+    resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
+
+def stock_price(ticker, start, end):
+    url = "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/" + ticker
+    resp = requests.get(url=url, params=statement_params)
+    output = resp.json()
+    return output['financials']
+
+def sector_info():
+    url = "https://financialmodelingprep.com/api/v3/stock/sectors-performance"
+    resp = requests.get(url=url)
+    output = resp.json()
+    return output
 
---
+++ Data/test.py
@@ -0,0 +1,6 @@
+import statements as statements
+
+result = statements.balance_sheet('AES')
+for entry in result:
+    print(entry)
+    print("-------------")
