2ad42583e900965f772f60b6b134860e83f2f194
Elikem Hermon <eliherm@gmail.com>
2019-11-15 17:46:57 -0500

    Adds plotting for close prices

--- Data/statements.py
+++ Data/statements.py
@@ -2,11 +2,13 @@
 import requests
 statement_params = {'period': 'quarter'}
 
+
 def income_statement(ticker):        
     url = "https://financialmodelingprep.com/api/v3/financials/income-statement/" + ticker
     resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
+
 
 def balance_sheet(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/" + ticker
@@ -14,13 +16,15 @@ def balance_sheet(ticker):
     output = resp.json()
     return output['financials']
 
+
 def cash_flow(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/" + ticker
     resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
 
-#start and end are formatted as yyyy-mm-dd
+
+# start and end are formatted as yyyy-mm-dd
 def stock_price(ticker, start, end):
     url = "https://financialmodelingprep.com/api/v3/historical-price-full/" + ticker
     time_param = {'from': start, 'to': end}
@@ -28,9 +32,9 @@ def stock_price(ticker, start, end):
     output = resp.json()
     return output['historical']
 
+
 def sector_info():
     url = "https://financialmodelingprep.com/api/v3/stock/sectors-performance"
     resp = requests.get(url=url)
     output = resp.json()
     return output['sectorPerformance']
-
--- Data/test.py
+++ Data/test.py
@@ -1,6 +1,14 @@
 import statements as statements
+import matplotlib.pyplot as plt
 
 result = statements.sector_info()
 for entry in result:
     print(entry)
     print("-------------")
+
+"""Plot the historical close prices"""
+plotResults = statements.stock_price("AAPL", "2018-03-12", "2019-11-12")
+plt.plot([entry['close'] for entry in plotResults])
+plt.ylabel("Price")
+plt.xlabel("Year")
+plt.show()
