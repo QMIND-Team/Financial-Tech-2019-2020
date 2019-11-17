f4b33d0ad2581bcd85aa0b8f8c129e0c4b9363cb
Ekimerton <ekim0252@gmail.com>
2019-11-17 18:31:00 -0500

    Added functionality to save data into a txt file, with json formatting

--- driver.py
+++ driver.py
@@ -1,15 +1,31 @@
 import pandas
+import json
 import Data.statements as statements
 
 result = pandas.read_csv('SandP500Companies.csv')
-for symbol in result['Symbol'][:10]:
+data = {}
+data['companies'] = []
+for idx, symbol in enumerate(result['Symbol'][:3]):
     try:
         income = statements.income_statement(symbol)
+        balance = statements.balance_sheet(symbol)
+        cash_flow = statements.cash_flow(symbol)
+        
+        # Quarterly stock prices calculated, put into a json list.
+        stock_price = []
         for entry in income:
             price = statements.quarterly_price(symbol, entry["date"])
-            entry["Quaterly Price"] = price
+            stock_price.append(price)
+        
+        company =   {'name':symbol, 'income_statement':income,
+                    'balance_sheet': balance, 'cash_flow': cash_flow,
+                    'stock_price': stock_price}
+        data['companies'].append(company)
+        print("Successfully read " + symbol + ", idx: " + str(idx))
+    except Exception as e:
+        print(e)
+        print("Info not found for ticker: " + symbol, ", idx: ", str(idx))
 
-        print(symbol)
-        print(income)
-    except:
-        print("Info not found for ticker: " + symbol)
+# Writing json to data.txt
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
