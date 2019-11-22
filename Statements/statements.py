c8bc90f32653df3281dc2e4ae33ea113a45520f8
Ekimerton <ekim0252@gmail.com>
2019-11-22 17:16:10 -0500

    Cleaned up home folder, fixed driver.py to work with more timeframes

--- Statements/statements.py
+++ Statements/statements.py
@@ -4,13 +4,11 @@ from datetime import datetime
 from datetime import timedelta
 statement_params = {'period': 'quarter'}
 
-
 def income_statement(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/income-statement/" + ticker
     resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
-
 
 def balance_sheet(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/" + ticker
@@ -18,13 +16,11 @@ def balance_sheet(ticker):
     output = resp.json()
     return output['financials']
 
-
 def cash_flow(ticker):
     url = "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/" + ticker
     resp = requests.get(url=url, params=statement_params)
     output = resp.json()
     return output['financials']
-
 
 # start and end are formatted as yyyy-mm-dd
 def stock_price(ticker, start, end):
@@ -34,17 +30,19 @@ def stock_price(ticker, start, end):
     output = resp.json()
     return output['historical']
 
-
 def sector_info():
     url = "https://financialmodelingprep.com/api/v3/stock/sectors-performance"
     resp = requests.get(url=url)
     output = resp.json()
     return output['sectorPerformance']
 
-
+# TODO: Add a way to conform dates with statement dates
 def quarterly_price(ticker, date):
+    # Turns string into datetime object
     date_list = date.split("-")
     date_mid = datetime(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+   
+    # Gets the prices for 20 days before and after the midpoint
     start = date_mid - timedelta(days=20)
     end = date_mid + timedelta(days=20)
     prices = stock_price(ticker, start.strftime("%Y-%m-%d"), end.strftime("%Y-%m-%d"))
@@ -52,6 +50,5 @@ def quarterly_price(ticker, date):
     quarter_total = 0.0
     for entry in prices:
         quarter_total += entry['close']
-
     quarter_price = quarter_total / len(prices)
     return quarter_price
--- driver.py
+++ driver.py
@@ -5,26 +5,30 @@ import Data.statements as statements
 result = pandas.read_csv('SandP500Companies.csv')
 data = {}
 data['companies'] = []
-for idx, symbol in enumerate(result['Symbol'][:3]):
+for idx, symbol in enumerate(result['Symbol']):
     try:
         income = statements.income_statement(symbol)
         balance = statements.balance_sheet(symbol)
         cash_flow = statements.cash_flow(symbol)
-        
-        # Quarterly stock prices calculated, put into a json list.
-        stock_price = []
-        for entry in income:
+    except:
+        print("Error with statements")
+        print("Skipped company: " + str(symbol) + ", idx: " + str(idx))
+
+    # Quarterly stock prices calculated, put into a json list.
+    stock_price = []
+    for entry in income:
+        try:
             price = statements.quarterly_price(symbol, entry["date"])
             stock_price.append(price)
+        except:
+            print("    Error with " + str(symbol) + ", date: " + entry["date"])
         
-        company =   {'name':symbol, 'income_statement':income,
-                    'balance_sheet': balance, 'cash_flow': cash_flow,
-                    'stock_price': stock_price}
-        data['companies'].append(company)
-        print("Successfully read " + symbol + ", idx: " + str(idx))
-    except Exception as e:
-        print(e)
-        print("Info not found for ticker: " + symbol, ", idx: ", str(idx))
+    # Saving the company to companies    
+    company =   {'name':symbol, 'income_statement':income,
+                'balance_sheet': balance, 'cash_flow': cash_flow,
+                'stock_price': stock_price}
+    data['companies'].append(company)
+    print("Successfully read " + symbol + ", idx: " + str(idx))
 
 # Writing json to data.txt
 with open('companies.json', 'w') as outfile:
