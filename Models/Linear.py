c20d59101da88b9ddedc29321e901acbc5933b78
Elikem Hermon <eliherm@gmail.com>
2019-11-29 19:43:06 -0500

    Adds linear regression test for stock data

--- Models/Linear.py
+++ Models/Linear.py
@@ -8,7 +8,6 @@ from sklearn.metrics import mean_squared_error, r2_score
 # Load the diabetes dataset
 diabetes = datasets.load_diabetes()
 
-
 # Use only one feature
 diabetes_X = diabetes.data[:, np.newaxis, 2]
 
