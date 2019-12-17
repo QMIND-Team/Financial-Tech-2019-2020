10bd62772281bfed5d6c92513524e7f8d12eb002
Ekimerton <ekim0252@gmail.com>
2019-12-16 20:42:00 -0500

    Finished data pull

--- driver.py
+++ driver.py
@@ -5,31 +5,47 @@ import Statements.statements as statements
 result = pandas.read_csv('Data/companies.csv')
 data = {}
 data['companies'] = []
-for idx, symbol in enumerate(result['Symbol'][:10]):
+
+for idx in range(len(result['Symbol'])):
+    symbol = result['Symbol'][idx]
+    industry = result['GICS�Sector'][idx]
+    
+    # Passes entry if the company is not listed under Utilities
+    if industry != "Utilities":
+        continue
+
+    # Checks to make sure the company's statements exist
     try:
         income = statements.income_statement(symbol)
         balance = statements.balance_sheet(symbol)
         cash_flow = statements.cash_flow(symbol)
     except:
-        print("Error with statements")
-        print("Skipped company: " + str(symbol) + ", idx: " + str(idx))
+        print("Couldn't find statements for: " + str(symbol) + ", idx: " + str(idx))
         continue
 
     # Quarterly stock prices calculated, put into a json list.
     stock_price = []
-    for entry in income:
+    for index, entry in enumerate(income):
         try:
             price = statements.quarterly_price(symbol, entry["date"])
             stock_price.append(price)
         except:
-            print("    Error with " + str(symbol) + ", date: " + entry["date"])
-        
-    # Saving the company to companies    
-    company =   {'name':symbol, 'income_statement':income,
-                'balance_sheet': balance, 'cash_flow': cash_flow,
-                'stock_price': stock_price}
-    data['companies'].append(company)
-    print("Successfully read " + symbol + ", idx: " + str(idx))
+            print("Error with " + str(symbol) + ", date: " + entry["date"])
+    
+    # Catch to check if all lists are the same length     
+    if (len(income) == len(balance) and len(income) == len(cash_flow) and len(income) == len(stock_price)):
+        # Saving the company to companies    
+        company =   {'name':symbol,
+                    'income_statement':income,
+                    'balance_sheet': balance,
+                    'cash_flow': cash_flow,
+                    'stock_price': stock_price
+                    }
+        data['companies'].append(company)
+
+        print("Successfully read " + symbol + ", idx: " + str(idx))
+    else:
+        print('Uneven data lengths for ' + symbol + ", idx:" + str(idx))
 
 # Writing json to data.txt
 with open('Data/companies.json', 'w') as outfile:
